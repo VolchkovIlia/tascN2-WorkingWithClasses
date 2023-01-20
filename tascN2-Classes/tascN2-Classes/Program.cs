c4bb281d8c121ab81aa91aa267894eeb17154ca0
VolchkovIlia <113889753+VolchkovIlia@users.noreply.github.com>
2023-01-20 13:44:05 +0300

    Add files via upload

---
+++ tascN2-Classes/tascN2-Classes/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using tascN2_Classes;
+Console.WriteLine("Результаты с браслетов участников конкурса за 10 дней:");
+Human[] steps = new Human[10];
+Random rand = new Random();
+
+for (int i = 0; i < steps.Length; i++)
+{
+    steps[i] = new Human(rand.Next(10000), rand.Next(10000), rand.Next(10000));
+    Console.WriteLine(steps[i]);
+}
---
+++ tascN2-Classes/tascN2-Classes/Steps.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tascN2_Classes
+{
+    internal class Human
+    {
+        private int steps1;
+        private int steps2;
+        private int steps3;
+
+        public int Steps1 { get => steps1; set => steps1 = value; }
+        public int Steps2 { get => steps2; set => steps2 = value; }
+        public int Steps3 { get => steps3; set => steps3 = value; }
+
+        public  Human(int steps1, int steps2, int steps3)
+        {
+            Steps1 = steps1;
+            Steps2 = steps2;
+            Steps3 = steps3;
+
+        }
+
+        public override string ToString()
+        {
+            return "Участники прошли за день: ( 1 - " + Steps1 + " шагов, 2 - " + Steps2 + "шагов , 3 - " + Steps3 + "шагов )";
+        }
+    }
+}
